12ae912a079af6df137d5790ef3736b3370403c4
a19osamn <60319141+A19osamn@users.noreply.github.com>
2020-04-16 11:46:35 +0200

    ListView funkar .

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] mountainNames = {"Qalamun","Hermun","Qasioun"};
+    private String[] mountainLocations = {"Damascus","Damascus","Sham"};
+    private int[] mountainsHeights= {2466,2814,1151};
+    private ArrayList<Mountain> mountainArrayList= new ArrayList<>();
+    private ArrayList<String> ListData=new ArrayList<>(Arrays.asList(mountainNames));
 
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = findViewById(R.id.myNewBtn);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>","We clicked on Tomten!");
+            }
+        });
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.list_item_textview,R.id.list_item_textview_xml,ListData);
+        ListView my_listview=(ListView) findViewById(R.id.my_listview);
+        my_listview.setAdapter(adapter);
+        my_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Enter your text here", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
