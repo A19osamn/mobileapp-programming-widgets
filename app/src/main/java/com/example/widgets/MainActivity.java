604d88222f58a778be6891beb089b297862beb36
a19osamn <60319141+A19osamn@users.noreply.github.com>
2020-04-21 13:17:13 +0200

    ListView funkar ,button funkar.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button b = findViewById(R.id.myNewBtn);
+
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
